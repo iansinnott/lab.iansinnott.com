73e98768f2ba342e61d344f5d6127fac86804709
Ian Sinnott <ian@iansinnott.com>
2016-12-30 00:01:34 -0800

    Improve styling

--- client/components/algs/AlgIndex.js
+++ client/components/algs/AlgIndex.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
+import classnames from 'classnames/bind';
+
+import s from './AlgIndex.styl';
+const cx = classnames.bind(s);
 
 export default class AlgIndex extends React.Component {
   render() {
     return (
-      <div className='AlgIndex'>
-        <h2>Here be algs</h2>
+      <div className={cx('AlgIndex', 'page')}>
+        <h1>Here be algs</h1>
 
-        <div className='list'>
+        <div className={cx('list')}>
           <Link to='/algs/insertion-sort'>Insertion Sort</Link>
         </div>
       </div>
