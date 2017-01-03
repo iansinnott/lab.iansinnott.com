9fe43c9c9d0931742e00a51dcfbecdee69684325
Ian Sinnott <ian@iansinnott.com>
2017-01-02 23:45:38 -0800

    Rewrite insertion sort with react entirely

--- client/components/algs/InsertionSort.js
+++ client/components/algs/InsertionSort.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { findDOMNode } from 'react-dom';
 import { Observable } from 'rxjs';
 import * as d3 from 'd3';
 import classnames from 'classnames/bind';
 import createDebugger from 'debug';
-import { identity } from 'ramda';
 
 const debug = createDebugger('alg-viz:components:InsertionSort'); // eslint-disable-line no-unused-vars
 
@@ -52,109 +50,65 @@ function quicksort(array) {
 // END TODO
 // -----------------------------------------------------------------------------
 
-// Try not to use `this` in here so that it can be easily refactored later.
-// svg: D3$Selection
-const renderSvg = (svg, props) => {
-  const { height, data, xScale, angleScale, margin } = props;
-
-  // NOTE: The units are crucial here since we are applying this as a CSS
-  // attribute. If we were using the SVG transform attribute the units would not
-  // matter, but they are very important when using CSS + SVG
-  const transform = (d, i) => {
-    return `translate(${xScale(i)}px,${height}px)rotate(${angleScale(d)}deg)`;
-  };
-
-  // We use a data join for the group to be declarative in our rendering. Since
-  // the data is always the same we can call renderSvg repeatedly and it will
-  // only render the group the first time
-  const group = svg.selectAll(`.${cx('lines')}`).data([null]);
-
-  const line = group
-    .enter().append('g') // Enter <g>
-      .attr('class', cx('lines'))
-      .attr('transform', `translate(${margin.left},${margin.top})`)
-    .merge(group)
-
-      // Data join. Use the actual number that's being sorted for the join
-      .selectAll('line')
-      .data(data, identity);
-
-  // NOTE: We use -height in order to make all the lines go UP, instead of
-  // down. This gives the appearane that they are bottom-aligned, so tha the
-  // bottom edge is a nice flat line while allt he rotated line endings are
-  // jagged at the top
-  line // Enter lines
-      .enter().append('line')
-        .attr('y2', -height) // See NOTE
-        .attr('stroke', 'white')
-        .style('transition-duration', '100ms')
-      .merge(line)
-        .style('transform', transform);
-};
-
 class Viz extends React.Component {
   static propTypes = {
     data: React.PropTypes.array.isRequired,
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
   };
 
   constructor(props) {
     super(props);
 
-    const { data } = props;
-
+    const { data, width } = props;
     const n = data.length;
-    const width = 600;
-    const height = 40;
+
+    this.xScale = d3.scalePoint()
+      .domain(d3.range(n))
+      .range([0, width]);
+
+    this.angleScale = d3.scaleLinear()
+      .domain([0, n - 1])
+      .range([-45, 45]);
+  }
+
+  // NOTE: The units are crucial here since we are applying this as a CSS
+  // attribute. If we were using the SVG transform attribute the units would not
+  // matter, but they are very important when using CSS + SVG
+  transform = (d, i) => {
+    const { xScale, angleScale } = this;
+    return `translate(${xScale(i)}px,${this.props.height}px)rotate(${angleScale(d)}deg)`;
+  };
+
+  render() {
+    const { data, width, height } = this.props;
     const margin = {
       left: 40,
       right: 40,
       top: 80,
       bottom: 80,
     };
-
     const outerWidth = width + margin.left + margin.right;
     const outerHeight = height + margin.top + margin.bottom;
 
-    const xScale = d3.scalePoint()
-      .domain(d3.range(n))
-      .range([0, width]);
-
-    const angleScale = d3.scaleLinear()
-      .domain([0, n - 1])
-      .range([-45, 45]);
-
-    this.svgProps = {
-      ...props,
-      width,
-      height,
-      outerWidth,
-      outerHeight,
-      margin,
-      xScale,
-      angleScale,
-    };
-  }
-
-  componentDidMount() {
-    this.svg = d3.select(findDOMNode(this));
-    this.svg.call(renderSvg, this.svgProps);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    debug('Re-rendering svg but not componenent');
-    this.svg.call(renderSvg, {
-      ...this.svgProps,
-      data: nextProps.data,
-    });
-
-    // Do not render the svg itself
-    return false;
-  }
-
-  render() {
-    const { outerWidth, outerHeight } = this.svgProps;
     return (
-      <svg width={outerWidth} height={outerHeight} />
+      <svg width={outerWidth} height={outerHeight}>
+        <g
+          className={cx('lines')}
+          transform={`translate(${margin.left},${margin.top})`}>
+          {data.map((x, i) => (
+            <line
+              key={x}
+              y2={-height}
+              stroke='white'
+              style={{
+                transitionDuration: '100ms',
+                transform: this.transform(x, i),
+              }}
+            />
+          ))}
+        </g>
+      </svg>
     );
   }
 }
@@ -210,7 +164,11 @@ export default class InsertionSort extends React.Component {
           <button className={cx('btn')} onClick={this.start}>Start</button>
         </div>
         <div className={cx('vizContainer')}>
-          <Viz data={this.state.data} />
+          <Viz
+            data={this.state.data}
+            width={600}
+            height={40}
+          />
         </div>
       </div>
     );
