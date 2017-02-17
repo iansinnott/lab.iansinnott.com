27259835db814e9a2846ae48dedc0d1e0c586fbf
Ian Sinnott <ian@iansinnott.com>
2017-02-17 00:15:53 -0800

    Add initial rxdb logo

--- client/components/Playground.js
+++ client/components/Playground.js
@@ -1,5 +1,5 @@
 /* @flow */
-import React from 'react';
+import React, { PropTypes as T } from 'react';
 import classnames from 'classnames/bind';
 import createDebugger from 'debug';
 
@@ -7,8 +7,49 @@ const debug = createDebugger('alg-viz:components:Playground'); // eslint-disable
 
 import s from './Playground.styl';
 const cx = classnames.bind(s);
+import { GridBox } from './Svg.js';
+import rxLogo from './rx-logo.png';
 
 const getThemeName = (bool) => bool ? 'light' : 'dark';
+
+class Cylinder extends React.Component {
+  static propTypes = {
+    size: T.number.isRequired,
+  };
+
+  static defaultProps = {
+    size: 180,
+  };
+
+  render() {
+    const { size } = this.props;
+    return (
+      <div style={{ width: 180, height: (90 * 0.77) }} className={cx('Cylinder')}>
+        <div
+          style={{ width: size, height: size }}
+          className={cx('cylinderContainer')}>
+          <div className={cx('cylinder')}>
+            <div className={cx('dot')} />
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class RxDbLogo extends React.Component {
+  render() {
+    return (
+      <div className={cx('RxDbLogo')}>
+        <Cylinder><img src={rxLogo} /></Cylinder>
+        <Cylinder />
+        <Cylinder />
+        <div className={cx('line')} />
+      </div>
+    );
+  }
+}
 
 class OptionsBox extends React.Component {
   state = {
@@ -39,9 +80,12 @@ export default class Playground extends React.Component {
         <h1>Playground</h1>
         <p>So much going on here!</p>
 
-        <OptionsBox title='RxDB Logo Concept'>
-          hey
-        </OptionsBox>
+        <GridBox
+          title='RxDB Logo Concept'
+          style={{ paddingTop: 100, paddingBottom: 100 }}
+          height={500}>
+          <RxDbLogo />
+        </GridBox>
       </div>
     );
   }
